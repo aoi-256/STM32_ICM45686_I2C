9717228bc25a17a7f5a234ee30379c8df81aac2e
aoi-256 <aoi256jp@Gmail.com>
2025-02-13 00:18:14 +0900

    Update ICM45686_HAL.cpp

--- ICM45686_HAL.cpp
+++ ICM45686_HAL.cpp
@@ -16,10 +16,10 @@ ICM45686_HAL::ICM45686_HAL(I2C_HandleTypeDef *use_i2cPin){
 
 void ICM45686_HAL::write(ICM45686_HAL::REGISTER REGISTER_ADDR, uint8_t* command, uint8_t len){
 
-	HAL_I2C_Mem_Write(i2c_pin, I2C_ADDR, uint8_t(REGISTER_ADDR), 1, command, len, 10);
+	HAL_I2C_Mem_Write(i2c_pin, I2C_ADDR, uint8_t(REGISTER_ADDR), 1, command, len, 1000);
 }
 
 void ICM45686_HAL::read(ICM45686_HAL::REGISTER REGISTER_ADDR, uint8_t* receive_buffer, uint8_t len){
 
-	HAL_I2C_Mem_Read(i2c_pin, I2C_ADDR, uint8_t(REGISTER_ADDR), 1, receive_buffer, len, 10);
+	HAL_I2C_Mem_Read(i2c_pin, I2C_ADDR, uint8_t(REGISTER_ADDR), 1, receive_buffer, len, 1000);
 }
